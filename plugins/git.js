e1dfd837b7b46231a0faf8a0b5a67d10809b788a
DARK UMAR <104460597+pindiboy404@users.noreply.github.com>
2022-05-21 21:22:28 +0500

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,22 +11,22 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/KITru9k.jpeg";
+  r_text[0] = "https://i.imgur.com/hlquIOG.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Ajmal & Achu*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Dark Umar*
 
 *TO CONTACT MY OWNERS
 
-ACHU : https://wa.me/918590967139 
+UMAR SER : https://wa.me/923060479256
 
-AJMAL : https://wa.me/919744196187*
+DARK UMAR : https://wa.me/919057593646*
 
-*githublink : https://github.com/Ajmal-Achu/Nandhutty*
+*githublink : https://github.com/pindiboy404/Dark-Umar-v2*
 
 *audio commads : https://github.com/Ajmal-Achu/media/tree/master/uploads*
 `}) 
